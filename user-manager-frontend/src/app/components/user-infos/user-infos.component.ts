c6204d307aebb9281e4ad8ce5af91c7e14a80bcd
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-26 18:52:12 -0300

    Criar userInfoComponent

---
+++ user-manager-frontend/src/app/components/user-infos/user-infos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-infos',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-infos.component.html',
+  styleUrl: './user-infos.component.scss'
+})
+export class UserInfosComponent {
+
+}
