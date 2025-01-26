fbd38a03ea0cfe0abd5cea23f5c24be1577b7012
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-26 18:57:22 -0300

    Adicionar configurações ao userInfosComponent

--- user-manager-frontend/src/app/components/user-infos/user-infos.component.ts
+++ user-manager-frontend/src/app/components/user-infos/user-infos.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-infos',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-infos.component.html',
   styleUrl: './user-infos.component.scss'
 })
 export class UserInfosComponent {
-
+  userInfosForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
 }
