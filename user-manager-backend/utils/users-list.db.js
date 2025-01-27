690d9caaa00c9494664b22768be42dfc6269ea11
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-27 14:46:38 -0300

    Criar um USER_LIST_DB

---
+++ user-manager-backend/utils/users-list.db.js
@@ -0,0 +1,16 @@
+const USERS_LIST_DB = [
+    {
+        name: 'john',
+        email: 'john@example.com',
+        username: 'john',
+        password: 'john',
+    },
+    {
+        name: 'laura',
+        email: 'laura@example.com',
+        username: 'laura',
+        password: 'laura',
+    },
+];
+
+module.exports = { USERS_LIST_DB };
