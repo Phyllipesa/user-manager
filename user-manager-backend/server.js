1ad33037a46203f7e8e7dcf869c90d3eb92bce64
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-27 14:47:38 -0300

    Configurar rota de login retornando um token

--- user-manager-backend/server.js
+++ user-manager-backend/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { USERS_LIST_DB } = require('./utils/users-list.db');
+const { generateTokenOnLogin } = require('./utils/jwt-manager');
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
-app.use(cors);
+app.use(cors());
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const USER_FOUND = USERS_LIST_DB.find(user => user.username === username && user.password === password);
+
+    if (!USER_FOUND) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const userToken = generateTokenOnLogin(username);
+
+    return res.json({ token: userToken });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
