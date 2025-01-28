3ff3decd070f3897203d185c9958eaf1ca31854e
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-28 15:27:42 -0300

    Configurar a rota put para atualização de usuarios

--- user-manager-backend/server.js
+++ user-manager-backend/server.js
@@ -31,6 +31,31 @@ app.post('/validate-token', authenticateToken, (req, res) => {
     res.json({ message: 'token válido', username: req.username });
 });
 
+app.put('/update-user', authenticateToken, (req, res) => {
+    const tokenUserName = req.username;
+    const newUserInfos = req.body;
+    const { name, email, username, password } = newUserInfos;
+
+    if (!name || !email || !username || !password) {
+        return res.status(400).json({ message: 'Al fields (name, email, usernam, password) are required.' });
+    }
+
+    const USER_FOUND = USERS_LIST_DB.findIndex((user) => user.username === tokenUserName);
+
+    if (USER_FOUND === -1) {
+        return res.status(404).json({ message: 'User not found.' });
+    }
+
+    USERS_LIST_DB[USER_FOUND] = newUserInfos;
+
+    const newToken = generateTokenOnLogin(username);
+
+
+
+    return res.status(200).json({ message: 'User updated successfully.', token: newToken });
+
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
