d1a7fb6727c0c78dadea148cc9b71c58ece51542
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-29 19:59:10 -0300

    Criar lógica para aplicação do token no authInterceptor

--- user-manager-frontend/src/app/interceptors/auth.interceptor.ts
+++ user-manager-frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,13 +1,21 @@
 import { HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
+
 import { Observable } from "rxjs";
+
+import { AUTH_TOKEN_ENABLED } from "../tokens/auth-token";
 
 export function authInterceptor(
     req: HttpRequest<unknown>,
-    next: HttpHandlerFn
-): Observable<HttpEvent<unknown>> {
+    next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
 
-    console.log('authInterceptor');
-    console.log('req', req);
+    let newReq = req;
+    const APPLY_AUTH_TOKEN = req.context.get(AUTH_TOKEN_ENABLED);
 
-    return next(req);
+    if (APPLY_AUTH_TOKEN) {
+        newReq = req.clone({
+            headers: req.headers.set('Authorization', localStorage.getItem('token')!),
+        });
+    }
+
+    return next(newReq);
 }
