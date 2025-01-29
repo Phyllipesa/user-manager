309b1cb9a74a36755d5baf31338352ea5f5b1762
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-29 20:00:07 -0300

    Aplicar lógica do context no LoginService

--- user-manager-frontend/src/app/services/login.service.ts
+++ user-manager-frontend/src/app/services/login.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpContext } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 import { map, Observable } from 'rxjs';
 
 import { ILoginResponse } from '../interfaces/response/login-response-interface';
+import { AUTH_TOKEN_ENABLED } from '../tokens/auth-token';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,17 @@ export class LoginService {
   private readonly _httpClient = inject(HttpClient);
 
   login(username: string, password: string): Observable<ILoginResponse> {
-    return this._httpClient.post<ILoginResponse>('http://localhost:3000/login', { username, password })
+    return this._httpClient
+      .post<ILoginResponse>(
+        'http://localhost:3000/login',
+        { 
+          username,
+          password,
+        },
+        { 
+          context: new HttpContext().set(AUTH_TOKEN_ENABLED, false)
+        },
+      )
       .pipe(
         map((tokenResponse) => {
           localStorage.setItem('token', 'Bearer ' + tokenResponse.token);
