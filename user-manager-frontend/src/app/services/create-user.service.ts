e074dcc084803ec57c03739b3886e98f016e52d8
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-29 20:00:15 -0300

    Aplicar lógica do context no CreateUserService

--- user-manager-frontend/src/app/services/create-user.service.ts
+++ user-manager-frontend/src/app/services/create-user.service.ts
@@ -1,7 +1,8 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpContext } from '@angular/common/http';
 
 import { IUserRequest } from '../interfaces/request/user-request.interface';
+import { AUTH_TOKEN_ENABLED } from '../tokens/auth-token';
 import { ICreateUserResponse } from '../interfaces/response/create-user-response.interface';
 
 @Injectable({
@@ -11,12 +12,12 @@ export class CreateUserService {
   private readonly _httpClient = inject(HttpClient);
 
   createUser(newUser: IUserRequest) {
-    const headers = new HttpHeaders().set('authorization', localStorage.getItem('token')!);
-    
     return this._httpClient.post<ICreateUserResponse>(
       'http://localhost:3000/create-user',
       newUser,
-      { headers, }
+      { 
+        context: new HttpContext().set(AUTH_TOKEN_ENABLED, true) 
+      },
     );
   }
 }
