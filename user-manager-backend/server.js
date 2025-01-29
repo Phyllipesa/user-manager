150cb7c0c2af93d5624f2584c2a0b8b26c96a615
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-29 12:24:35 -0300

    Configurar a rota post para criação de usuario

--- user-manager-backend/server.js
+++ user-manager-backend/server.js
@@ -43,7 +43,7 @@ app.put('/update-user', authenticateToken, (req, res) => {
     const USER_FOUND = USERS_LIST_DB.findIndex((user) => user.username === tokenUserName);
 
     if (USER_FOUND === -1) {
-        return res.status(404).json({ message: 'User not found.' });
+        return res.status(403).json({ message: 'User not found.' });
     }
 
     USERS_LIST_DB[USER_FOUND] = newUserInfos;
@@ -54,8 +54,35 @@ app.put('/update-user', authenticateToken, (req, res) => {
 
     return res.status(200).json({ message: 'User updated successfully.', token: newToken });
 
-})
+});
+
+app.post('/create-user', authenticateToken, (req, res) => {
+    const tokenUserName = req.username;
+    const newUser = req.body;
+    const { name, email, username, password } = newUser;
+
+    if (!name || !email || !username || !password) {
+        return res.status(400).json({ message: 'Al fields (name, email, usernam, password) are required.' });
+    }
+
+    const USER_TOKEN_FOUND = USERS_LIST_DB.findIndex((user) => user.username === tokenUserName);
+
+    if (USER_TOKEN_FOUND === -1) {
+        return res.status(403).json({ message: 'User not found.' });
+    }
+
+    const USER_FOUND = USERS_LIST_DB.findIndex((user) => user.username === username);
+    const USER_ALREADY_EXISTS = USER_FOUND !== -1;
+
+    if (USER_ALREADY_EXISTS) {
+        return res.status(409).json({ message: 'User already exists.' });
+    }
+
+    USERS_LIST_DB.push(newUser);
+
+    return res.status(201).json({ message: 'User created successfully.' });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
