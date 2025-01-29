f37bd2be4b11de16c7820c356d885f9c7ac5aae7
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-29 20:00:28 -0300

    Aplicar lógica do context no UpdateUserService

--- user-manager-frontend/src/app/services/update-user.service.ts
+++ user-manager-frontend/src/app/services/update-user.service.ts
@@ -1,10 +1,11 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpContext } from '@angular/common/http';
 
 import { map } from 'rxjs';
 
 import { IUserRequest } from '../interfaces/request/user-request.interface';
-import { IUpdateUserResponde } from '../interfaces/response/update-user-response.interface';
+import { AUTH_TOKEN_ENABLED } from '../tokens/auth-token';
+import { IUpdateUserResponse } from '../interfaces/response/update-user-response.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +14,19 @@ export class UpdateUserService {
   private readonly _httpClient = inject(HttpClient);
 
   updateUser(userInfos: IUserRequest) {
-    const headers = new HttpHeaders().set('authorization', localStorage.getItem('token')!);
-    return this._httpClient.put<IUpdateUserResponde>('http://localhost:3000/update-user', userInfos, {
-      headers,
-    }).pipe(
-      map((updateUserResponse) => {
-        localStorage.setItem('token', 'Bearer ' + updateUserResponse.token);
-        return updateUserResponse;
-      })
-    );
+    return this._httpClient
+      .put<IUpdateUserResponse>(
+        'http://localhost:3000/update-user',
+        userInfos,
+        { 
+          context: new HttpContext().set(AUTH_TOKEN_ENABLED, true)
+        },
+      )
+      .pipe(
+        map((updateUserResponse) => {
+          localStorage.setItem('token', 'Bearer ' + updateUserResponse.token);
+          return updateUserResponse;
+        })
+      );
   }
 }
