28ead73b952ab3b42b37a0c7e62ca7989ef0ae18
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-29 13:22:55 -0300

    Adicionar tipagens IUserRequest e IUpdateUserResponse ao método updateUser do UpdateService

--- user-manager-frontend/src/app/services/update-user.service.ts
+++ user-manager-frontend/src/app/services/update-user.service.ts
@@ -3,15 +3,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { map } from 'rxjs';
 
+import { IUserRequest } from '../interfaces/request/user-request.interface';
+import { IUpdateUserResponde } from '../interfaces/response/update-user-response.interface';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateUserService {
   private readonly _httpClient = inject(HttpClient);
 
-  updateUser(userInfos: { name: string; email: string; username: string; password: string; }) {
+  updateUser(userInfos: IUserRequest) {
     const headers = new HttpHeaders().set('authorization', localStorage.getItem('token')!);
-    return this._httpClient.put<{ message: string; token: string; }>('http://localhost:3000/update-user', userInfos, {
+    return this._httpClient.put<IUpdateUserResponde>('http://localhost:3000/update-user', userInfos, {
       headers,
     }).pipe(
       map((updateUserResponse) => {
