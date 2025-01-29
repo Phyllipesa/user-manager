2c27803e6dd7273d1b3c62ba77f108a83f09eaa4
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-29 12:57:53 -0300

    Criar e configurar o método createUser do userInfosComponent

--- user-manager-frontend/src/app/components/user-infos/user-infos.component.ts
+++ user-manager-frontend/src/app/components/user-infos/user-infos.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 import { UpdateUserService } from '../../services/update-user.service';
+import { CreateUserService } from '../../services/create-user.service';
 
 @Component({
   selector: 'app-user-infos',
@@ -18,6 +21,7 @@ export class UserInfosComponent {
   });
 
   private readonly _updateUserService = inject(UpdateUserService);
+  private readonly _createUserService = inject(CreateUserService);
   
   updateUser() {
     this._updateUserService.updateUser(this.userInfosForm.value as any).subscribe({
@@ -26,6 +30,24 @@ export class UserInfosComponent {
       },
       error: (error) => {
         this.userInfosForm.setErrors({ 'update-error': true })
+      },
+    });
+  }
+
+  createUser() {
+    this._createUserService.createUser(this.userInfosForm.value as any).subscribe({
+      next: (response) => {
+        this.userInfosForm.setErrors({ 'create-user-success': true })
+      },
+      error: (error: HttpErrorResponse) => {
+        const ALREADY_EXISTING_USER = error.status === 409;
+
+        if (ALREADY_EXISTING_USER) {
+          this.userInfosForm.setErrors({ 'existing-user-error': true })
+          return;
+        }
+        
+        this.userInfosForm.setErrors({ 'created-user-error': true })
       },
     });
   }
