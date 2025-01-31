eb8fb66a03173a154df4db4c60a6ac733aa05eee
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-30 21:38:02 -0300

    Fazer injeção de dependencia do loadingService

--- user-manager-frontend/src/app/app.component.ts
+++ user-manager-frontend/src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingService } from './services/loading.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'user-manager-frontend';
+  readonly _loadingService = inject(LoadingService);
 }
