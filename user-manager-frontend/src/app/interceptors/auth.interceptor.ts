244c58a5fb02004f6c94ae33bdec2545bfcde678
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-30 21:03:12 -0300

    Refatoração: Adicionar lógica para caso o token não seja encontrado

--- user-manager-frontend/src/app/interceptors/auth.interceptor.ts
+++ user-manager-frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,21 +1,24 @@
 import { HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
 
-import { Observable } from "rxjs";
+import { Observable, throwError } from "rxjs";
 
 import { AUTH_TOKEN_ENABLED } from "../tokens/auth-token";
 
 export function authInterceptor(
     req: HttpRequest<unknown>,
-    next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
-
-    let newReq = req;
+    next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> {
+    
     const APPLY_AUTH_TOKEN = req.context.get(AUTH_TOKEN_ENABLED);
+    const TOKEN = APPLY_AUTH_TOKEN ? localStorage.getItem('token') : null;
 
-    if (APPLY_AUTH_TOKEN) {
-        newReq = req.clone({
-            headers: req.headers.set('Authorization', localStorage.getItem('token')!),
-        });
+    if (APPLY_AUTH_TOKEN && !TOKEN) {
+        return throwError(() => new Error('Token not found.'));
     }
+
+    const newReq = TOKEN ? req.clone({
+        headers: req.headers.set('Authorization', TOKEN),
+    }) : req;
 
     return next(newReq);
 }
