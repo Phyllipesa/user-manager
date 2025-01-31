40039018ea61127c57477b0ad0af3b9936cdbe14
Phyllipe <phyllipe.sjc@hotmail.com>
2025-01-30 21:34:30 -0300

    Adicionar delay nas requisições de login e create-user do servidor

--- user-manager-backend/server.js
+++ user-manager-backend/server.js
@@ -14,17 +14,19 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    const USER_FOUND = USERS_LIST_DB.find(user => user.username === username && user.password === password);
-
-    if (!USER_FOUND) {
-        return res.status(401).json({ message: 'Invalid credentials' });
-    }
-
-    const userToken = generateTokenOnLogin(username);
-
-    return res.json({ token: userToken });
+    setTimeout(() => {
+        const { username, password } = req.body;
+    
+        const USER_FOUND = USERS_LIST_DB.find(user => user.username === username && user.password === password);
+    
+        if (!USER_FOUND) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+    
+        const userToken = generateTokenOnLogin(username);
+    
+        return res.json({ token: userToken });
+    }, 2000)
 });
 
 app.post('/validate-token', authenticateToken, (req, res) => {
@@ -57,30 +59,32 @@ app.put('/update-user', authenticateToken, (req, res) => {
 });
 
 app.post('/create-user', authenticateToken, (req, res) => {
-    const tokenUserName = req.username;
-    const newUser = req.body;
-    const { name, email, username, password } = newUser;
-
-    if (!name || !email || !username || !password) {
-        return res.status(400).json({ message: 'Al fields (name, email, usernam, password) are required.' });
-    }
-
-    const USER_TOKEN_FOUND = USERS_LIST_DB.findIndex((user) => user.username === tokenUserName);
-
-    if (USER_TOKEN_FOUND === -1) {
-        return res.status(403).json({ message: 'User not found.' });
-    }
-
-    const USER_FOUND = USERS_LIST_DB.findIndex((user) => user.username === username);
-    const USER_ALREADY_EXISTS = USER_FOUND !== -1;
-
-    if (USER_ALREADY_EXISTS) {
-        return res.status(409).json({ message: 'User already exists.' });
-    }
-
-    USERS_LIST_DB.push(newUser);
-
-    return res.status(201).json({ message: 'User created successfully.' });
+    setTimeout(() => {
+        const tokenUserName = req.username;
+        const newUser = req.body;
+        const { name, email, username, password } = newUser;
+    
+        if (!name || !email || !username || !password) {
+            return res.status(400).json({ message: 'Al fields (name, email, usernam, password) are required.' });
+        }
+    
+        const USER_TOKEN_FOUND = USERS_LIST_DB.findIndex((user) => user.username === tokenUserName);
+    
+        if (USER_TOKEN_FOUND === -1) {
+            return res.status(403).json({ message: 'User not found.' });
+        }
+    
+        const USER_FOUND = USERS_LIST_DB.findIndex((user) => user.username === username);
+        const USER_ALREADY_EXISTS = USER_FOUND !== -1;
+    
+        if (USER_ALREADY_EXISTS) {
+            return res.status(409).json({ message: 'User already exists.' });
+        }
+    
+        USERS_LIST_DB.push(newUser);
+    
+        return res.status(201).json({ message: 'User created successfully.' });
+    }, 2000)
 });
 
 app.listen(PORT, () => {
